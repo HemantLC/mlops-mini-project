aa9d14a0e90163dfd2b50ed2967c622f91efb985
Hemant MUkherjee <hemantrec@gmail.com>
2026-05-04 19:33:46 +0530

    add experiment 1 LR baseline

---
+++ notebooks/dagshub-setup-test.py
@@ -0,0 +1,8 @@
+import dagshub
+mlflow.set_tracking_uri("https://dagshub.com/HemantLC/mlops-mini-project.mlflow")
+dagshub.init(repo_owner='HemantLC', repo_name='mlops-mini-project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
