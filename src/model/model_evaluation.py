ae697050e39527b20686e4f4e8dee01a15f4d841
Hemant MUkherjee <hemantrec@gmail.com>
2026-05-06 19:19:39 +0530

    added flask app code

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -25,7 +25,7 @@ import os
 # Set up MLflow tracking URI
 #mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-#import dagshub
+import dagshub
 
 mlflow.set_tracking_uri("https://dagshub.com/HemantLC/mlops-mini-project.mlflow")
 dagshub.init(repo_owner='HemantLC', repo_name='mlops-mini-project', mlflow=True)
