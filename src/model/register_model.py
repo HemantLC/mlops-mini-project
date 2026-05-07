2f2032167f0e635eff4c2f015935cc1706256b77
Hemant MUkherjee <hemantrec@gmail.com>
2026-05-08 00:57:38 +0530

    updated model_evaluation.py code

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -15,15 +15,15 @@ if not dagshub_token:
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-dagshub_url = "https://dagshub.com/HemantLC/mlops-mini-project.mlflow"
-repo_owner = "HemantLC"
-repo_name = "mlops-mini-project"
+#dagshub_url = "https://dagshub.com/HemantLC/mlops-mini-project.mlflow"
+#repo_owner = "HemantLC"
+#repo_name = "mlops-mini-project"
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+#mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-#mlflow.set_tracking_uri("https://dagshub.com/HemantLC/mlops-mini-project.mlflow")
-#dagshub.init(repo_owner='HemantLC', repo_name='mlops-mini-project', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/HemantLC/mlops-mini-project.mlflow")
+dagshub.init(repo_owner='HemantLC', repo_name='mlops-mini-project', mlflow=True)
 
 # logging configuration
 logger = logging.getLogger('model_registration')
